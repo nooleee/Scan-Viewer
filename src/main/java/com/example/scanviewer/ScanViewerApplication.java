6946984b96f8fe49740f49a4202e8d845da9d295
nooleee <wjssnfl7739@naver.com>
2024-06-21 14:53:16 +0900

    Create Project

---
+++ src/main/java/com/example/scanviewer/ScanViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scanviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scanviewer/ScanViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scanviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
