c45617207e92a0aab4e35644f0bcf2aabdd8af11
mingi <mingikim.giv@gmail.com>
2024-06-25 15:09:15 +0900

    ADD: ReportRepository 생성

--- src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
+++ src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
@@ -1,7 +1,5 @@
 package com.pacs.scanviewer.SCV.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> { }
