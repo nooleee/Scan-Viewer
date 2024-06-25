adbb3fc6470c26cc5b03bc64f2ac7e0679480453
mingi <mingikim.giv@gmail.com>
2024-06-25 17:00:02 +0900

    ADD: ReportService 생성

---
+++ src/main/java/com/pacs/scanviewer/SCV/service/ReportService.java
@@ -0,0 +1,19 @@
+package com.pacs.scanviewer.SCV.service;
+
+import com.pacs.scanviewer.SCV.domain.Report;
+import com.pacs.scanviewer.SCV.domain.ReportRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ReportService {
+    private final ReportRepository reportRepository;
+
+    public List<Report> findAll() {
+        return reportRepository.findAll();
+    }
+
+}
