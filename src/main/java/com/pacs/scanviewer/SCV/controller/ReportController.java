1694a04fc62cb8c270db466918c741c0d5e50007
mingi <mingikim.giv@gmail.com>
2024-06-25 17:56:49 +0900

    Modify: ReportController(createReport)

--- src/main/java/com/pacs/scanviewer/SCV/controller/ReportController.java
+++ src/main/java/com/pacs/scanviewer/SCV/controller/ReportController.java
@@ -1,7 +1,21 @@
 package com.pacs.scanviewer.SCV.controller;
 
-import org.springframework.stereotype.Controller;
+import com.pacs.scanviewer.SCV.domain.Report;
+import com.pacs.scanviewer.SCV.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/report")
 public class ReportController {
+    private final ReportService reportService;
+
+    @PostMapping
+    public ResponseEntity<Report> createReport(@RequestBody Report report) {
+        Report savedReport = reportService.save(report);
+        return new ResponseEntity<>(savedReport, HttpStatus.CREATED);
+    }
 }
