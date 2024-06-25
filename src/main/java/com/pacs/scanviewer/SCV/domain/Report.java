ecb4d6c83a1d5646dbc3c4ce8b039c137a0d0a65
mingi <mingikim.giv@gmail.com>
2024-06-25 14:55:55 +0900

    ADD: Report 생성

--- src/main/java/com/pacs/scanviewer/SCV/domain/Report.java
+++ src/main/java/com/pacs/scanviewer/SCV/domain/Report.java
@@ -1,34 +1,21 @@
 package com.pacs.scanviewer.SCV.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Getter
-@Setter
 @Table(name = "report")
 @Entity
 public class Report {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int studyKey;
-
     private String userCode;
-
     private String content;
     private String patient;
     private String date;
     private String diseaseCode;
-
-    @Enumerated(EnumType.STRING)
     private String videoReplay;
-
     private Timestamp regDate;
     private Timestamp modDate;
-
-    public enum String {
-        읽지않음, 판독불가, 판독완료
-    }
 }
