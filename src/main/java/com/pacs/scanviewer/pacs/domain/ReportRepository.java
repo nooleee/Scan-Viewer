6ecd2e47f1da2a57e10a7a16b8ee8581dd2f9964
mingi <mingikim.giv@gmail.com>
2024-06-25 14:09:28 +0900

    DELETE

--- src/main/java/com/pacs/scanviewer/pacs/domain/ReportRepository.java
+++
@@ -1,6 +0,0 @@
-package com.pacs.scanviewer.pacs.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Integer> {
-}
