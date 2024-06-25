d63efdd5c82782690907c69860912c2acd7a6477
mingi <mingikim.giv@gmail.com>
2024-06-25 14:56:06 +0900

    ADD: ReportController 생성

--- src/main/java/com/pacs/scanviewer/SCV/controller/ReportController.java
+++ src/main/java/com/pacs/scanviewer/SCV/controller/ReportController.java
@@ -1,45 +1,7 @@
 package com.pacs.scanviewer.SCV.controller;
 
-import com.pacs.scanviewer.SCV.domain.Report;
-import com.pacs.scanviewer.SCV.service.ReportService;
-import com.pacs.scanviewer.pacs.domain.Study;
-import com.pacs.scanviewer.pacs.service.StudyService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/report")
+@Controller
 public class ReportController {
-    private final ReportService reportService;
-    private final StudyService studyService;
-
-    @GetMapping()
-    public ModelAndView getAllReports() {
-        ModelAndView model = new ModelAndView("report/report");
-        List<Study> reportList = studyService.getAllStudies();
-        model.addObject("reports", reportList);
-        return model;
-    }
-
-    @PostMapping
-    public String createReport(@ModelAttribute Report report) {
-        reportService.save(report);
-        return "redirect:/report";
-    }
-
-    @PutMapping("/update")
-    public String updateReport(@ModelAttribute Report report) {
-        reportService.update(report);
-        return "redirect:/report";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteReport(@PathVariable int id) {
-        reportService.delete(id);
-        return "redirect:/report";
-    }
 }
