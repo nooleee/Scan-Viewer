385bea2347dbed0d3cedce8d8a9e960490f6efb5
mingi <mingikim.giv@gmail.com>
2024-06-25 17:23:48 +0900

    Modify: ReportRepository @추가

--- src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
+++ src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
@@ -1,5 +1,7 @@
 package com.pacs.scanviewer.SCV.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Integer> { }
