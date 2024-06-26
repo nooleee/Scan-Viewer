4213cba51180bc513e8c5c765044aba4fa703708
ryuuki98 <ryuuki98@naver.com>
2024-06-26 14:08:12 +0900

    create: /studies/{studykey}를 입력받아 해당 스터디의 시리즈와 이미지 정보를 받아오는 getImageNamesByStudyKey 완성

--- src/main/java/com/pacs/scanviewer/pacs/controller/StudyController.java
+++ src/main/java/com/pacs/scanviewer/pacs/controller/StudyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
@@ -27,16 +28,20 @@ public class StudyController {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @ResponseBody
-    @GetMapping("/studies/{page}")
-    public ResponseEntity<List<Study>> getAllStudies(@PathVariable int page,@PageableDefault(size = 5) Pageable pageable) {
-        List<Study> result = studyService.findAllWithPage(page - 1, pageable);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
 //    @ResponseBody
-//    @GetMapping("/studies/{studykey}")
-//    public ResponseEntity<Study> getStudy(@PathVariable String studykey) {
-//
+//    @GetMapping("/studies/{page}")
+//    public ResponseEntity<List<Study>> getAllStudies(@PathVariable int page,@PageableDefault(size = 5) Pageable pageable) {
+//        List<Study> result = studyService.findAllWithPage(page - 1, pageable);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
 //    }
+
+    @ResponseBody
+    @GetMapping("/studies/{studykey}")
+    public ResponseEntity<Map<Long, List<String>>> getStudy(@PathVariable long studykey) {
+        Map<Long, List<String>> imageNamesByStudyKey = studyService.getImageNamesByStudyKey(studykey);
+
+        ResponseEntity<Map<Long, List<String>>> res = new ResponseEntity(imageNamesByStudyKey,HttpStatus.OK);
+
+        return res;
+    }
 }
