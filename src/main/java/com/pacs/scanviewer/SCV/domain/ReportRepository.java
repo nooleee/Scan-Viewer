e69b09e0ced091a87a59295de58e51c5d9292fca
mingi <mingikim.giv@gmail.com>
2024-06-26 11:05:19 +0900

    Modify: ReportRepository(Integer -> Long)

--- src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
+++ src/main/java/com/pacs/scanviewer/SCV/domain/ReportRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Integer> { }
+public interface ReportRepository extends JpaRepository<Report, Long> { }
