b601020145885230f9863969735e486318030b25
nooleee <wjssnfl7739@naver.com>
2024-06-27 14:18:34 +0900

    Modify 이미지 관련 수정

--- src/main/resources/static/webpack.config.js
+++ src/main/resources/static/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
     entry: './script/cornerstone.js',
@@ -14,5 +15,32 @@ module.exports = {
         type: 'filesystem',
         cacheDirectory: path.resolve(__dirname, '.webpack_cache'),
     },
-    mode: 'development'
+    mode: 'development',
+
+    experiments: {
+        asyncWebAssembly: true // 또는 syncWebAssembly: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.wasm$/,
+                type: 'webassembly/async' // 또는 'webassembly/sync'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.wasm$/,
+                use: 'file-loader',
+                type: 'javascript/auto'
+            }
+        ]
+    },
+    plugins: [
+        new NodePolyfillPlugin()
+    ]
+
+
 };
