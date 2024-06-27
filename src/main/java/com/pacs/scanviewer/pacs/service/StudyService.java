f36942491cb6c1857e7a9746f511902fbf0dc6b1
nooleee <wjssnfl7739@naver.com>
2024-06-27 15:05:41 +0900

    Modify getImageNamesByStudyKey 수정

--- src/main/java/com/pacs/scanviewer/pacs/service/StudyService.java
+++ src/main/java/com/pacs/scanviewer/pacs/service/StudyService.java
@@ -2,6 +2,7 @@ package com.pacs.scanviewer.pacs.service;
 
 import com.pacs.scanviewer.pacs.domain.Image;
 //import com.pacs.scanviewer.pacs.domain.ImageRepository;
+import com.pacs.scanviewer.pacs.domain.ImageRepository;
 import com.pacs.scanviewer.pacs.domain.Study;
 import com.pacs.scanviewer.pacs.domain.StudyRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class StudyService {
 
     private final StudyRepository studyRepository;
+    private final ImageRepository imageRepository;
 //    private final ImageRepository imageRepository;
 
     public List<Study> getAllStudies() {
@@ -35,6 +39,18 @@ public class StudyService {
     public List<Study> getStudiesByPid(String pid) {
         return studyRepository.findAllByPid(pid);
     }
+
+    public Map<Long, List<String>> getImageNamesByStudyKey(Long studyKey) {
+        List<Image> imageList = imageRepository.findByStudykey(studyKey);
+        Map<Long, List<String>> seriesImages = new HashMap<>();
+
+        for (Image image : imageList) {
+            seriesImages.computeIfAbsent(image.getSerieskey(), k -> new java.util.ArrayList<>()).add(image.getPath() + image.getFname());
+        }
+
+        return seriesImages;
+    }
+
 
 //    public List<Image> findByStudykey(long studykey) {
 //        return imageRepository.findAllById(studykey);
