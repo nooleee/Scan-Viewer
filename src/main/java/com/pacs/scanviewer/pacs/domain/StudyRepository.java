7608d72c743896231e31649e584105b6520eb351
k0gang <k0gang@naver.com>
2024-06-27 14:26:39 +0900

    add: findAllByPid

--- src/main/java/com/pacs/scanviewer/pacs/domain/StudyRepository.java
+++ src/main/java/com/pacs/scanviewer/pacs/domain/StudyRepository.java
@@ -3,6 +3,10 @@ package com.pacs.scanviewer.pacs.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    List<Study> findAllByPid(String pid);
 }
