6564a77d4de8441e4e0bdc4dcae02217ad661066
k0gang <k0gang@naver.com>
2024-06-27 17:28:26 +0900

    ADD: 부분 문자열 검색을 위한 containing 메소드 추가

--- src/main/java/com/pacs/scanviewer/pacs/domain/StudyRepository.java
+++ src/main/java/com/pacs/scanviewer/pacs/domain/StudyRepository.java
@@ -1,5 +1,7 @@
 package com.pacs.scanviewer.pacs.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+    Page<Study> findByPidContaining(String pid, Pageable pageable);
+    Page<Study> findByPnameContaining(String pname, Pageable pageable);
 
     List<Study> findAllByPid(String pid);
 }
