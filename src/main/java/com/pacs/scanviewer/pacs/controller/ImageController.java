53453d9b08a29137d81c65204adb56b71d1fb511
ryuuki98 <ryuuki98@naver.com>
2024-06-28 17:59:24 +0900

    modify: user service 수정

--- src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
+++ src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
@@ -20,6 +20,8 @@ import org.springframework.web.servlet.ModelAndView;
 import java.awt.print.Pageable;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -68,58 +70,58 @@ public class ImageController {
     }
 
 
-    // window용 코드
     @GetMapping("/dicom-file")
     @ResponseBody
-    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) {
-        File file = new File(path);
+    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) throws IOException {
+        String os = System.getProperty("os.name").toLowerCase();
+        String sep = System.getProperty("file.separator");
+
+
+
+        // URL 디코딩
+        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8.name());
+
+        String rootPath = "Z:\\STS\\";
+
+        if (!os.contains("win")) {
+            rootPath = "/Volumes/STS/";
+           decodedPath = decodedPath.replace("Z:/",rootPath);
+        }
+
+        // 역슬래시를 슬래시로 변경
+        decodedPath = decodedPath.replace("\\", sep);
+
+        // 절대 경로인지 확인
+        String fullPath;
+        if (decodedPath.startsWith("Z:") || decodedPath.startsWith("/Volumes/STS")) {
+            // 이미 절대 경로인 경우 rootPath를 추가하지 않음
+            fullPath = decodedPath;
+        } else {
+            // 상대 경로인 경우 rootPath를 추가
+            fullPath = rootPath + decodedPath;
+        }
+
+        // 전체 파일 경로 생성
+        File file = new File(fullPath);
+
+        // 디버깅을 위한 로그 추가
+        System.out.println("Requested path: " + path);
+        System.out.println("Decoded path: " + decodedPath);
+        System.out.println("Full file path: " + file.getAbsolutePath());
+
+        // 파일이 존재하지 않을 경우 404 응답
         if (!file.exists()) {
+            System.out.println("File not found: " + file.getAbsolutePath()); // 파일이 존재하지 않을 경우 로그 추가
             return ResponseEntity.notFound().build();
         }
 
+        // 파일이 존재할 경우 파일 반환
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getName() + "\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(new FileSystemResource(file));
     }
-
-    // mac용 코드
-//    @GetMapping("/dicom-file")
-//    @ResponseBody
-//    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) throws IOException {
-//        // URL 디코딩
-//        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8.name());
-//
-//        // 경로 앞에 /Volumes/STS가 이미 붙어있는 경우 중복 추가하지 않기 위해 검사
-//        String basePath = "/Volumes/STS";
-//        String fullPath;
-//        if (decodedPath.startsWith(basePath)) {
-//            fullPath = decodedPath;
-//        } else {
-//            fullPath = basePath + decodedPath;
-//        }
-//
-//        // 역슬래시를 슬래시로 변경
-//        fullPath = fullPath.replace("\\", "/");
-//
-//        // 전체 파일 경로 생성
-//        File file = new File(fullPath);
-//
-//        // 디버깅을 위한 로그 추가
-//        System.out.println("Requested path: " + path);
-//        System.out.println("Decoded path: " + decodedPath);
-//        System.out.println("Full file path: " + file.getAbsolutePath());
-//
-//        // 파일이 존재하지 않을 경우 404 응답
-//        if (!file.exists()) {
-//            System.out.println("File not found: " + file.getAbsolutePath()); // 파일이 존재하지 않을 경우 로그 추가
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        // 파일이 존재할 경우 파일 반환
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getName() + "\"")
-//                .body(new FileSystemResource(file));
-//    }
-
 }
+
+
+
