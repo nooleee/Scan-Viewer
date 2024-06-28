30d5d190a46c0c7259e00ea435d4d40636ff6818
nooleee <wjssnfl7739@naver.com>
2024-06-28 18:05:30 +0900

    Delete 미사용 메소드 삭제

--- src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
+++ src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
@@ -126,42 +126,4 @@ public class ImageController {
     }
 }
 
-    // mac용 코드
-//    @GetMapping("/dicom-file")
-//    @ResponseBody
-//    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) throws IOException {
-//        // URL 디코딩
-//        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8.name());
-//
-//        // 경로 앞에 /Volumes/STS가 이미 붙어있는 경우 중복 추가하지 않기 위해 검사
-//        String basePath = "/Volumes/STS";
-//        String fullPath;
-//        if (decodedPath.startsWith(basePath)) {
-//            fullPath = decodedPath;
-//        } else {
-//            fullPath = basePath + decodedPath;
-//        }
-//
-//        // 역슬래시를 슬래시로 변경
-//        fullPath = fullPath.replace("\\", "/");
-//
-//        // 전체 파일 경로 생성
-//        File file = new File(fullPath);
-//
-//        // 디버깅을 위한 로그 추가
-//        System.out.println("Requested path: " + path);
-//        System.out.println("Decoded path: " + decodedPath);
-//        System.out.println("Full file path: " + file.getAbsolutePath());
-//
-//        // 파일이 존재하지 않을 경우 404 응답
-//        if (!file.exists()) {
-//            System.out.println("File not found: " + file.getAbsolutePath()); // 파일이 존재하지 않을 경우 로그 추가
-//            return ResponseEntity.notFound().build();
-//        }
-//
-//        // 파일이 존재할 경우 파일 반환
-//        return ResponseEntity.ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getName() + "\"")
-//                .body(new FileSystemResource(file));
-//    }
 
