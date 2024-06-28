e39075f67aeb6fdc878a91b1d3ed33358642bd11
nooleee <wjssnfl7739@naver.com>
2024-06-28 17:18:26 +0900

    Modify imageService에서 사용하던 메소드 이동 및 수정

--- src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
+++ src/main/java/com/pacs/scanviewer/pacs/controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.pacs.scanviewer.pacs.controller;
 
 import com.pacs.scanviewer.pacs.domain.Image;
+import com.pacs.scanviewer.pacs.domain.ImageRepository;
 import com.pacs.scanviewer.pacs.domain.Study;
 import com.pacs.scanviewer.pacs.service.ImageService;
 import com.pacs.scanviewer.pacs.service.StudyService;
@@ -20,9 +21,12 @@ import org.springframework.web.servlet.ModelAndView;
 import java.awt.print.Pageable;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequestMapping("/images")
 @RequiredArgsConstructor
@@ -41,19 +45,19 @@ public class ImageController {
 
     @GetMapping("/dicom-images")
     public ResponseEntity<List<Image>> getDicomImages() {
-        List<Image> dicomImages = imageService.getDicomImages();
+        List<Image> allImages = imageService.getDicomImages();
+        List<Image> dicomImages = allImages.stream()
+                .filter(image -> image.getFname().endsWith(".dcm"))
+                .collect(Collectors.toList());
         return new ResponseEntity<>(dicomImages, HttpStatus.OK);
     }
 
     @GetMapping("/{studykey}/{serieskey}")
     public ModelAndView getImagesByStudyKeyAndSeriesKey(@PathVariable Long studykey, @PathVariable Long serieskey) {
-//        Map<Long, List<String>> seriesImages = imageService.getImageNamesByStudyKey(studykey);
-//        List<String> images = seriesImages.getOrDefault(serieskey, new ArrayList<>());
-//
-//        ModelAndView mv = new ModelAndView("viewer/viewer");
-//        mv.addObject("images", images);
-        List<String> images = imageService.getDicomUrlsByStudyKeyAndSeriesKey(studykey, serieskey);
-        System.out.println("images size : " + images.size());
+        List<Image> imageList = imageService.getDicomUrlsByStudyKeyAndSeriesKey(studykey, serieskey);
+        List<String> images = imageList.stream()
+                .map(image -> "Z:/" + image.getPath() + image.getFname())
+                .collect(Collectors.toList());
         ModelAndView mv = new ModelAndView("viewer/viewer");
         mv.addObject("images", images);
         return mv;
@@ -62,26 +66,65 @@ public class ImageController {
     @GetMapping("/{studykey}/{serieskey}/dicom-urls")
     @ResponseBody
     public ResponseEntity<List<String>> getDicomUrlsByStudyKeyAndSeriesKey(@PathVariable Long studykey, @PathVariable Long serieskey) {
-        List<String> dicomUrls = imageService.getDicomUrlsByStudyKeyAndSeriesKey(studykey, serieskey);
-        System.out.println("dicomUrls size : " + dicomUrls.size());
+        List<Image> imageList = imageService.getDicomUrlsByStudyKeyAndSeriesKey(studykey, serieskey);
+        List<String> dicomUrls = imageList.stream()
+                .map(image -> "Z:/" + image.getPath() + image.getFname())
+                .collect(Collectors.toList());
+
         return new ResponseEntity<>(dicomUrls, HttpStatus.OK);
     }
 
-
-    // window용 코드
     @GetMapping("/dicom-file")
     @ResponseBody
-    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) {
-        File file = new File(path);
+    public ResponseEntity<FileSystemResource> getDicomFile(@RequestParam String path) throws IOException {
+        String os = System.getProperty("os.name").toLowerCase();
+        String sep = System.getProperty("file.separator");
+        // URL 디코딩
+        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8.name());
+
+        String rootPath = "Z:\\STS\\";
+
+        if (!os.contains("win")) {
+            rootPath = "/Volumes/STS/";
+            decodedPath = decodedPath.replace(rootPath, "");
+        }
+
+
+        // 역슬래시를 슬래시로 변경
+        decodedPath = decodedPath.replace("\\", sep);
+        System.out.println("decodedPath : " + decodedPath);
+
+        // 절대 경로인지 확인
+        String fullPath;
+        if (decodedPath.startsWith("Z:") || decodedPath.startsWith("/Volumes/STS")) {
+            // 이미 절대 경로인 경우 rootPath를 추가하지 않음
+            fullPath = decodedPath;
+        } else {
+            // 상대 경로인 경우 rootPath를 추가
+            fullPath = rootPath + decodedPath;
+        }
+
+        // 전체 파일 경로 생성
+        File file = new File(fullPath);
+
+        // 디버깅을 위한 로그 추가
+        System.out.println("Requested path: " + path);
+        System.out.println("Decoded path: " + decodedPath);
+        System.out.println("Full file path: " + file.getAbsolutePath());
+
+        // 파일이 존재하지 않을 경우 404 응답
         if (!file.exists()) {
+            System.out.println("File not found: " + file.getAbsolutePath()); // 파일이 존재하지 않을 경우 로그 추가
             return ResponseEntity.notFound().build();
         }
 
+        // 파일이 존재할 경우 파일 반환
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + file.getName() + "\"")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(new FileSystemResource(file));
     }
+}
 
     // mac용 코드
 //    @GetMapping("/dicom-file")
@@ -122,4 +165,3 @@ public class ImageController {
 //                .body(new FileSystemResource(file));
 //    }
 
-}
