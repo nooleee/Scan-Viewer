484c43f0f63622298dd266aec438783224ec1b75
nooleee <wjssnfl7739@naver.com>
2024-06-29 15:04:30 +0900

    Add findSeriesKeysByStudyKey 쿼리 메소드 추가

--- src/main/java/com/pacs/scanviewer/pacs/domain/ImageRepository.java
+++ src/main/java/com/pacs/scanviewer/pacs/domain/ImageRepository.java
@@ -1,6 +1,7 @@
 package com.pacs.scanviewer.pacs.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByStudykeyAndSerieskey(long studykey, long serieskey);
+
+    @Query("SELECT DISTINCT i.serieskey FROM Image i WHERE i.studykey = :studykey")
+    List<Long> findSeriesKeysByStudyKey(Long studykey);
 }
