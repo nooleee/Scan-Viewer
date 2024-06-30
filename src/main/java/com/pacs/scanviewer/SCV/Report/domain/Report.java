8ff992a5785687709491b56eba763acdff6ff456
k0gang <k0gang@naver.com>
2024-06-30 16:05:52 +0900

    Add: Annotation EntityListeners

--- src/main/java/com/pacs/scanviewer/SCV/Report/domain/Report.java
+++ src/main/java/com/pacs/scanviewer/SCV/Report/domain/Report.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -13,6 +14,7 @@ import java.sql.Timestamp;
 @Setter
 @Table(name = "report")
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Report {
     @Id
     private int studyKey;
