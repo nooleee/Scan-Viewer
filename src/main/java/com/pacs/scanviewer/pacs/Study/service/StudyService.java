69c56ff09815bccba71167eaaa4450cb34d2263e
k0gang <k0gang@naver.com>
2024-07-01 15:07:08 +0900

    StudySpecification 을 사용해 불필요한 메소드 정리

--- src/main/java/com/pacs/scanviewer/pacs/Study/service/StudyService.java
+++ src/main/java/com/pacs/scanviewer/pacs/Study/service/StudyService.java
@@ -3,6 +3,7 @@ package com.pacs.scanviewer.pacs.Study.service;
 //import com.pacs.scanviewer.pacs.Image.domain.ImageRepository;
 import com.pacs.scanviewer.pacs.Study.domain.Study;
 import com.pacs.scanviewer.pacs.Study.domain.StudyRepository;
+import com.pacs.scanviewer.pacs.Study.domain.StudySpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,14 +19,6 @@ public class StudyService {
     private final StudyRepository studyRepository;
 //    private final ImageRepository imageRepository;
 
-    public List<Study> getAllStudies() {
-        return studyRepository.findAll();
-    }
-
-    public List<Study> findAllWithPage(int page, Pageable pageable) {
-        return studyRepository.findAll(pageable.withPage(page)).getContent();
-    }
-
     public Page<Study> findStudiesWithPage(int page, int size) {
         return studyRepository.findAllByOrderByStudydateDesc(PageRequest.of(page, size));
     }
@@ -34,26 +27,10 @@ public class StudyService {
         return studyRepository.findAllByPid(pid);
     }
 
-    public List<Study> findByStudykey(long studykey) {
-        return studyRepository.findAllByStudykey(studykey);
-    }
 
-    public Page<Study> findByPidContainingAndDateRange(String pid, String startDate, String endDate, PageRequest pageRequest) {
-        return studyRepository.findByPidContainingAndStudydateBetweenOrderByStudydateDesc(pid, startDate, endDate, pageRequest);
+    public Page<Study> searchStudies(String pid, String pname, String startDate, String endDate, String modality, Pageable pageable) {
+        return studyRepository.findAll(StudySpecification.searchStudies(pid, pname, startDate, endDate, modality), pageable);
     }
-
-    public Page<Study> findByPnameContainingAndDateRange(String pname, String startDate, String endDate, PageRequest pageRequest) {
-        return studyRepository.findByPnameContainingAndStudydateBetweenOrderByStudydateDesc(pname, startDate, endDate, pageRequest);
-    }
-
-    public Page<Study> findByPidPnameAndDateRange(String pid, String pname, String startDate, String endDate, PageRequest pageRequest) {
-        return studyRepository.findByPidContainingAndPnameContainingAndStudydateBetweenOrderByStudydateDesc(pid, pname, startDate, endDate, pageRequest);
-    }
-
-    public Page<Study> findByDateRange(String startDate, String endDate, PageRequest pageRequest) {
-        return studyRepository.findByStudydateBetweenOrderByStudydateDesc(startDate, endDate, pageRequest);
-    }
-
 
 
 //    public List<Image> findByStudykey(long studykey) {
