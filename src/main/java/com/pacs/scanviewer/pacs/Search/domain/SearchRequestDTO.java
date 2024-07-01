a5864e8678d21069a6478a851f35f11a404910de
k0gang <k0gang@naver.com>
2024-07-01 17:04:22 +0900

    ADD : SearchRequestDTO

---
+++ src/main/java/com/pacs/scanviewer/pacs/Search/domain/SearchRequestDTO.java
@@ -0,0 +1,50 @@
+package com.pacs.scanviewer.pacs.Search.domain;
+
+public class SearchRequestDTO {
+    private String pid;
+    private String pname;
+    private String startDate;
+    private String endDate;
+    private String modality;
+
+    // Getters and Setters
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getModality() {
+        return modality;
+    }
+
+    public void setModality(String modality) {
+        this.modality = modality;
+    }
+}
