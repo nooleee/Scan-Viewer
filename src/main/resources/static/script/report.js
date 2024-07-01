d67d74f4b157dc95616390aac59ef08082f59d50
mingi <mingikim.giv@gmail.com>
2024-07-01 17:58:50 +0900

    Modify: report.js(ICD 코드 검색)

--- src/main/resources/static/script/report.js
+++ src/main/resources/static/script/report.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
             diseaseCode: $('#diseaseCode').val(),
             content: $('#content').val(),
             patient: $('#patient').val(),
-            videoReplay: $('#videoReplay').val(),  // 필요에 따라 적절한 값을 넣어줍니다.
+            videoReplay: $('#videoReplay').val(),
         };
 
         console.log('전송할 데이터:', reportData);
@@ -23,11 +23,40 @@ $(document).ready(function () {
                 console.log('성공 응답:', response);
                 alert("리포트가 생성되었습니다.");
                 location.href="/worklist";
-                // 필요한 경우 UI를 업데이트합니다.
             },
             error: function (xhr, status, error) {
                 console.error('에러 응답:', xhr, status, error);
                 alert("리포트 생성을 실패했습니다.");
+            }
+        });
+    });
+
+    $('#searchICDButton').on('click', function () {
+        var query = $('#diseaseCode').val();
+        $.ajax({
+            type: "GET",
+            url: "/report/searchICD",
+            data: { query: query },
+            success: function (response) {
+                console.log('ICD 코드 검색 결과:', response);
+                if (response === "검색 결과가 없습니다.") {
+                    alert("ICD 코드 검색 결과가 없습니다.");
+                } else {
+                    // 검색 결과를 표시합니다.
+                    $('#icdResults').html('<ul>' + response.split(',').map(function(item) {
+                        return '<li class="icd-result-item">' + item + '</li>';
+                    }).join('') + '</ul>');
+
+                    // 검색 결과 항목에 클릭 이벤트 핸들러 추가
+                    $('.icd-result-item').on('click', function () {
+                        $('#diseaseCode').val($(this).text());
+                        $('#icdResults').empty();  // 검색 결과 목록 비우기
+                    });
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error('ICD 코드 검색 에러:', xhr, status, error);
+                alert("ICD 코드 검색에 실패했습니다.");
             }
         });
     });
