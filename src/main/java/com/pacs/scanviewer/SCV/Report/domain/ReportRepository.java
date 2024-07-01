cf032c93186e9c741abacaffa581147142c5d550
mingi <mingikim.giv@gmail.com>
2024-07-01 12:40:03 +0900

    Add: ReportRepository(deleteById)

--- src/main/java/com/pacs/scanviewer/SCV/Report/domain/ReportRepository.java
+++ src/main/java/com/pacs/scanviewer/SCV/Report/domain/ReportRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Integer> {
+public interface ReportRepository extends JpaRepository<Report, ReportId> {
 
     Report findByStudyKey(int studyKey);
+    void deleteById(ReportId id);
 }
