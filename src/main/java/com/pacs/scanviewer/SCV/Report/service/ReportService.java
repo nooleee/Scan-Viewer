31c64a289ab05be8bd6b560e8a0d703bcce3f030
mingi <mingikim.giv@gmail.com>
2024-07-01 17:56:50 +0900

    Modify: ReportService(searchICDCode Method add)

--- src/main/java/com/pacs/scanviewer/SCV/Report/service/ReportService.java
+++ src/main/java/com/pacs/scanviewer/SCV/Report/service/ReportService.java
@@ -4,12 +4,18 @@ import com.pacs.scanviewer.SCV.Report.domain.Report;
 import com.pacs.scanviewer.SCV.Report.domain.ReportId;
 import com.pacs.scanviewer.SCV.Report.domain.ReportRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ReportService {
     private final ReportRepository reportRepository;
+    private final ICDAPIclient icdAPIclient;
 
     public Report save(Report report) {
         return reportRepository.save(report);
@@ -29,4 +35,25 @@ public class ReportService {
     public Report getReportByStudyKey(int studyKey) {
         return reportRepository.findByStudyKey(studyKey);
     }
+
+    public String searchICDCode(String query) throws Exception {
+        String token = icdAPIclient.getToken();
+        String response = icdAPIclient.getICDCode(token, query);
+
+        JSONObject jsonResponse = new JSONObject(response);
+        JSONArray destinationEntities = jsonResponse.optJSONArray("destinationEntities");
+
+        if (destinationEntities == null || destinationEntities.length() == 0) {
+            return "검색 결과가 없습니다.";
+        } else {
+            List<String> results = new ArrayList<>();
+            for (int i = 0; i < destinationEntities.length(); i++) {
+                JSONObject entity = destinationEntities.getJSONObject(i);
+                String title = entity.getString("title").replaceAll("\\<.*?\\>", ""); // HTML 태그 제거
+                results.add(title);
+            }
+            return results.toString();
+        }
+    }
+
 }
