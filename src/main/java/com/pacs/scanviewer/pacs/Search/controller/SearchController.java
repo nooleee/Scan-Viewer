42126628cc8b20062789ff6d8524364deee14a07
k0gang <k0gang@naver.com>
2024-07-02 14:12:31 +0900

    modify : 토큰에서 userCode 찾기

--- src/main/java/com/pacs/scanviewer/pacs/Search/controller/SearchController.java
+++ src/main/java/com/pacs/scanviewer/pacs/Search/controller/SearchController.java
@@ -1,5 +1,9 @@
 package com.pacs.scanviewer.pacs.Search.controller;
 
+import com.pacs.scanviewer.SCV.Consent.domain.Consent;
+import com.pacs.scanviewer.SCV.Consent.service.ConsentService;
+import com.pacs.scanviewer.Util.CookieUtil;
+import com.pacs.scanviewer.Util.JwtUtil;
 import com.pacs.scanviewer.pacs.Search.domain.SearchRequestDTO;
 import com.pacs.scanviewer.pacs.Search.domain.SearchResponseDTO;
 import com.pacs.scanviewer.pacs.Search.service.SearchService;
@@ -9,12 +13,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
     private final SearchService searchService;
+    private final JwtUtil jwtUtil;
+    private final ConsentService consentService;
 
     @CrossOrigin
     @GetMapping("/studies")
@@ -22,7 +31,20 @@ public class SearchController {
     public Page<SearchResponseDTO> searchStudies(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size,
-            SearchRequestDTO searchDTO) {
-        return searchService.searchStudies(searchDTO, PageRequest.of(page, size));
+            SearchRequestDTO searchDTO, HttpServletRequest request) {
+        String token = CookieUtil.getCookieValue(request, "jwt");
+        String userCode = jwtUtil.extractUsername(token);
+
+        Page<SearchResponseDTO> searchResponseDTOS = searchService.searchStudies(searchDTO, PageRequest.of(page, size));
+
+        searchResponseDTOS.forEach(dto -> {
+            Optional<Consent> consent = consentService.findConsentByStudyKeyAndUserCode((int)dto.getStudykey(), userCode);
+            if(consent.isPresent()) {
+                dto.setExamstatus(1);
+            }else{
+                dto.setExamstatus(0);
+            }
+        });
+        return searchResponseDTOS;
     }
 }
