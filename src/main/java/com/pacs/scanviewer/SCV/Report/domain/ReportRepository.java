eab229af54814e57cc5c796b2398486f4020d7f3
mingi <mingikim.giv@gmail.com>
2024-07-03 18:25:00 +0900

    Add: ReportRepository(findReportByStudyKey Method)

--- src/main/java/com/pacs/scanviewer/SCV/Report/domain/ReportRepository.java
+++ src/main/java/com/pacs/scanviewer/SCV/Report/domain/ReportRepository.java
@@ -3,8 +3,11 @@ package com.pacs.scanviewer.SCV.Report.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report, ReportId> {
 
     Report findByStudyKey(int studyKey);
+    Optional<Report> findReportByStudyKey(int studykey);
 }
