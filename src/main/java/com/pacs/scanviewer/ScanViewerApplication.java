91054fa827a959c6cd96a124a15bd3468968f0bb
mingi <mingikim.giv@gmail.com>
2024-07-12 18:34:31 +0900

    Add: ScanViewerApplication(builder)

--- src/main/java/com/pacs/scanviewer/ScanViewerApplication.java
+++ src/main/java/com/pacs/scanviewer/ScanViewerApplication.java
@@ -2,6 +2,8 @@ package com.pacs.scanviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +11,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
-public class ScanViewerApplication {
+public class ScanViewerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ScanViewerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ScanViewerApplication.class, args);
